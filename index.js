ee144328dcff1b62473bd20044d0a22d7a6cc492
yuxichen11@gmail.com <yuxichen11@gmail.com>
2018-04-17 04:05:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var recipes = {};
-function UpdateObjectWithKeyAndValue(obj, key, value) {
+function updateObjectWithKeyAndValue(obj, key, value) {
   return Object.assign({},obj,{[key]:value})
 }  
 function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
